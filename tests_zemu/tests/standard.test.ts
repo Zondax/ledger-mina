419efeaefebeaa10259c7bdae30e48cef1069586
0xPxt <raventosaleix@gmail.com>
2025-02-06 23:07:17 +0100

    Bump Version

--- tests_zemu/tests/standard.test.ts
+++ tests_zemu/tests/standard.test.ts
@@ -69,7 +69,7 @@ describe('Standard', function () {
       const resp = await app.getAppVersion()
       console.log(resp)
 
-      expect(resp.version).toEqual('1.4.1')
+      expect(resp.version).toEqual('1.4.2')
     } finally {
       await sim.close()
     }
