916a4146ca4def53eb218951e26f7de4b7a0f5b8
Carlos Medeiros <carlos.medeiros@zondax.ch>
2026-04-17 13:04:57 +0100

    improvement: update zemu tests configuration and dependencies
    Update Jest configuration to handle ESM-only dependencies like `get-port` and bump multiple devDependencies (including Zemu, Ledgerhq, and ESLint plugins) to their latest versions.

--- tests_zemu/jest.config.js
+++ tests_zemu/jest.config.js
@@ -1,7 +1,28 @@
+// ESM-only dependencies that must be transpiled to CommonJS before Jest
+// can require() them. `get-port` went ESM-only in v6; @zondax/zemu still
+// require()s it. Add more names here if another transitive dep ships ESM.
+const esmDeps = ['get-port']
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  transformIgnorePatterns: ['^.+\\.js$'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.m?js$': [
+      'ts-jest',
+      {
+        useESM: false,
+        isolatedModules: true,
+        tsconfig: {
+          allowJs: true,
+          esModuleInterop: true,
+          module: 'CommonJS',
+          target: 'ES2020',
+        },
+      },
+    ],
+  },
+  transformIgnorePatterns: [`/node_modules/(?!(${esmDeps.join('|')})/)`],
   reporters: ['default', ['summary', { summaryThreshold: 1 }]],
   globalSetup: './globalsetup.js',
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
