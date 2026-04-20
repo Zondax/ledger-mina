4a97663d524f05e4ae8bfb152b1bdd8d728566ad
Carlos Medeiros <carlos.medeiros@zondax.ch>
2026-04-20 14:28:59 +0100

    bump version

--- tests_zemu/tests/standard.test.ts
+++ tests_zemu/tests/standard.test.ts
@@ -65,7 +65,7 @@ describe('Standard', function () {
       const resp = await app.getAppVersion()
       console.log(resp)
 
-      expect(resp.version).toEqual('1.6.4')
+      expect(resp.version).toEqual('1.6.5')
     } finally {
       await sim.close()
     }
